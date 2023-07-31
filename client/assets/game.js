0ddb3cc8c2e23836fa60a8a99e30d2ae1321e914
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-07-31 10:01:01 +0100

    add tilemap + update loop

--- client/assets/game.js
+++ client/assets/game.js
@@ -1,2 +1,21 @@
+
+//ESTABLISHES CANVAS CONTEXT AND LOCATION.
 const canvas = document.getElementById('gameCanvas');
-console.log(canvas);
+const ctx = canvas.getContext('2d');
+
+//TILEMAP 
+const tileW = 40;
+const tileH = 40;
+
+const gridRows = 10;
+const gridCols = 10;
+
+//Makes a continouos loop so game can be refreshed. Think of framerate.
+const updateAll = () => {
+    window.requestAnimationFrame(updateAll);
+}
+
+//RUNS FUNCTION WHEN WINDOW HAS LOADED.
+window.onload = () => {
+    window.requestAnimationFrame(updateAll);
+}
